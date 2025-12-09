cee82a2360a7483c75157fadcc19fc05f69cdb9d
Y.C. Tsai <yctsaiyc@gmail.com>
2025-12-09 16:43:09 +0800

    Product

---
+++ Product.py
@@ -0,0 +1,13 @@
+from WC import WC
+from Uni import Uni
+import pandas as pd
+
+
+if __name__ == "__main__":
+    wc = WC()
+    uni = Uni()
+
+    df = pd.concat([wc.get_product_df(), uni.get_product_df()], axis=0)
+    csv_path = "data/product.csv"
+    df.to_csv(csv_path, index=False, encoding="utf-8-sig")
+    print(f"Saved: {csv_path}")
